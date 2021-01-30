3d0efe52aef89edfa07e6e9cc7fd4929e9165876
cynthiateeters <glean@cynthiateeters.com>
2021-01-30 15:13:21 -0500

    Fix typos

--- test/conditionals.test.js
+++ test/conditionals.test.js
@@ -8,63 +8,63 @@ const {
 } = require('../src/conditionals');
 
 
-test('should be 2 for the odds in 0,1,2,3,4', () => {
+test('Should be 2 for the odds in 0,1,2,3,4', () => {
   expect(numberOfOdds(5)).toBe(2);
 });
 
-test('should be 66 for the odds in 0->132', () => {
+test('Should be 66 for the odds in 0->132', () => {
   expect(numberOfOdds(132)).toBe(66);
 });
 
-test('should be F for grade 55', () => {
+test('Should be F for grade 55', () => {
   expect(gradeGenerator(55)).toBe('F');
 });
 
-test('should be D for grade 60', () => {
+test('Should be D for grade 60', () => {
   expect(gradeGenerator(60)).toBe('D');
 });
 
-test('should be C for grade 75', () => {
+test('Should be C for grade 75', () => {
   expect(gradeGenerator(75)).toBe('C');
 });
 
-test('should be B for grade 82', () => {
+test('Should be B for grade 82', () => {
   expect(gradeGenerator(82)).toBe('B');
 });
 
-test('should be A for grade 99', () => {
+test('Should be A for grade 99', () => {
   expect(gradeGenerator(99)).toBe('A');
 });
 
-test('should be Francine got an A', () => {
+test('Should be Francine got an A', () => {
   expect(getGrade('Francine',99)).toBe('Francine got an A');
 });
 
-test('should be David got a B', () => {
+test('Should be David got a B', () => {
   expect(getGrade('David',87)).toBe('David got a B');
 });
 
 
-test('should be No breakfast!', () => {
+test('Should be No breakfast!', () => {
   expect(riceKrispies(1)).toBe('No breakfast!');
 });
 
-test('should be Snap!', () => {
+test('Should be Snap!', () => {
   expect(riceKrispies(33)).toBe('Snap!');
 });
 
-test('should be Snap! and Crackle!', () => {
+test('Should be Snap! and Crackle!', () => {
   expect(riceKrispies(15)).toBe('Snap! and Crackle!');
 });
 
-test('should be Snap! and Pop!', () => {
+test('Should be Snap! and Pop!', () => {
   expect(riceKrispies(21)).toBe('Snap! and Pop!');
 });
 
-test('should be Snap! and Pop!', () => {
+test('Should be Snap! and Pop!', () => {
   expect(riceKrispies(70)).toBe('Crackle! and Pop!');
 });
 
-test('should be Snap!, Crackle! and Pop!', () => {
+test('Should be Snap!, Crackle! and Pop!', () => {
   expect(riceKrispies(210)).toBe('Snap!, Crackle! and Pop!');
 });
--- test/geometry.test.js
+++ test/geometry.test.js
@@ -9,28 +9,28 @@ const {
 } = require('../src/geometry');
 
 
-test('should be the perimeter of rectangle to 2 places', () => {
+test('Should be the perimeter of rectangle to 2 places', () => {
   expect(perimeterRectangle(5, 2)).toBe(14);
 });
 
-test('should be the area of rectangle to 2 places', () => {
+test('Should be the area of rectangle to 2 places', () => {
   expect(areaRectangle(5, 2)).toBe(10);
 });
 
-test('should be the circumference of circle to 2 places', () => {
+test('Should be the circumference of circle to 2 places', () => {
   expect(circumferenceCircle(5)).toBe(31.42);
 });
 
-test('should be the area of circle to 2 places', () => {
+test('Should be the area of circle to 2 places', () => {
   expect(areaCircle(5)).toBe(78.54);
 });
 
 
-test('should be hypothenuse of triangle to 2 places', () => {
+test('Should be hypothenuse of triangle to 2 places', () => {
   expect(pythagorean(3,4)).toBe(5);
 });
 
-test('should be hypothenuse of triangle to 2 places', () => {
+test('Should be hypothenuse of triangle to 2 places', () => {
   expect(pythagorean(8,12)).toBe(14.42);
 });
 
