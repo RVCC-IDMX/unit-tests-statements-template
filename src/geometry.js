a054ecff6483d5320ef9877960dcb27bb84587e7
cynthia <glean@cynthiateeters.com>
2021-09-16 21:09:02 -0400

    Add stuff

--- src/geometry.js
+++ src/geometry.js
@@ -1,59 +1,75 @@
 /* geometry.js */
 
-function perimeterRectangle(width, height) {
-  // return the perimeter of the rectangle with 
-  // sides width and height
-  // to 2 decimal places
-  // Hint: use toFixed()
+/**
+ * return the perimeter of the rectangle with
+ * sides width and height
+ * to 2 decimal places
+ *
+ * @param {number} width
+ * @param {number} height
+ * @returns {number}
+ *
+ * Hint: you can use toFixed()
+ */
+
+function rectanglePerimeter(width, height) {
   // write your code here
-  
 }
 
-function areaRectangle(width, height) {
-  // return the perimeter of the rectangle with 
-  // sides width and height
-  // to 2 decimal places
-  // Hint: use toFixed()
+/**
+ * return the area of the rectangle with
+ * sides width and height
+ * to 2 decimal places
+ *
+ * @param {number} width
+ * @param {number} height
+ * @returns {number}
+ *
+ * Hint: you can use toFixed()
+ */
+function rectangleArea(width, height) {
   // write your code here
-
 }
 
-function circumferenceCircle(radius) {
-  // return the circumference of a circle with 
-  // a radius
-  // c = 2 * pi * radius
-  // to 2 decimal places
-  // Hint: use toFixed()
+/**
+ * return the circumference of a circle with a known radius
+ * to 2 decimal places
+ * @param {number} radius
+ * @returns {number}
+ *
+ * The math equation is c = 2 * pi * radius
+ *
+ * Hint: use toFixed()
+ */
+function circleCircumference(radius) {
   // write your code here
-
 }
 
-function areaCircle(radius) {
-  // return the area of a circle with 
-  // a radius
-  // a = pi * the square of the radius 
-  // to 2 decimal places
-  // Hint: use toFixed()
+/**
+ * return the area of a circle with a known radius
+ * to 2 decimal places
+ *
+ * @param {number} radius
+ * @returns {number}
+ */
+function circleArea(radius) {
   // write your code here
-
 }
 
-
+/**
+ *
+ * @param {number} sideA
+ * @param {number} sideB
+ * @returns {number}
+ */
 function pythagorean(sideA, sideB) {
-  // return the length of a triangle's
-  // hypotenuse given the lengths of the
-  // sides using the Pythagorean theorem
-  // to 2 decimal places
-  // Hint: use toFixed()
   // write your code here
-  
 }
-
 
 module.exports = {
   perimeterRectangle,
   areaRectangle,
   circumferenceCircle,
   areaCircle,
-  pythagorean
+  pythagorean,
 };
